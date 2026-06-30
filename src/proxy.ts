aaefc3d85bfbed5b1550382b5a2bacb918765373
Gabriel Demargne <gabriel@demargne.com>
2026-06-30 13:49:47 +0200

    Fix domain redirect loop

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,20 +1,6 @@
-import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-const WWW_HOST = "www.gabrieldemargne.com";
-const APEX_HOST = "gabrieldemargne.com";
-
-export function proxy(request: NextRequest) {
-  const host = request.headers.get("host");
-
-  if (host === WWW_HOST) {
-    const redirectUrl = request.nextUrl.clone();
-    redirectUrl.host = APEX_HOST;
-    redirectUrl.protocol = "https:";
-    redirectUrl.port = "";
-    return NextResponse.redirect(redirectUrl, 308);
-  }
-
+export function proxy() {
   return NextResponse.next();
 }
 
